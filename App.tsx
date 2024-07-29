27a8b48d58ad444d2b15a6ddcb1e00ac2687d7ab
MinMyatPaing <minmyatpaing64@gmail.com>
2024-07-29 12:49:40 +0100

    initalized app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+type StatComponentContainerProps = {
+  label: string;
+  value: string;
+};
+
+const StatComponentContainer = ({
+  label,
+  value,
+}: StatComponentContainerProps) => {
+  return (
+    <View style={styles.valueContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <Text style={styles.value}>{value}</Text>
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Worlds!</Text>
+
+      <View style={{ flexDirection: "row" }}>
+        <StatComponentContainer label="Steps" value="1219" />
+        <StatComponentContainer label="Flights Climbed" value="12" />
+      </View>
+
+      <StatComponentContainer label="Distance" value="0,75 km" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    justifyContent: "center",
+    padding: 12,
+  },
+  valueContainer: {
+    marginRight: 50,
+    marginVertical: 10,
+  },
+  label: {
+    color: "white",
+    fontSize: 20,
+  },
+  value: {
+    fontSize: 35,
+    color: "#AFB3BE",
+    fontWeight: "500",
   },
 });
